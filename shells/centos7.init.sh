c2ca4b5ff7e7a3817f34b396478d77b49b81d962
lc4t <lc4t0.0@gmail.com>
2019-08-06 20:59:37 +0800

    update

--- shells/centos7.init.sh
+++ shells/centos7.init.sh
@@ -5,7 +5,7 @@ yum groupinstall -y "Development tools"
 yum install -y python36-devel python36-pip
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum install docker-ce
+yum install docker-ce -y
 pip3 install docker-compose requests
 chsh -s /usr/bin/fish
 yum update -y
