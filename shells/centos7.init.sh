8f718bc04e7572bc02f7cf0eb988d33806725817
lc4t <lc4t0.0@gmail.com>
2019-08-06 19:51:14 +0800

    centos7 init

---
+++ shells/centos7.init.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+yum install -y tmux fish git epel-release net-tools vim openssl-devel
+yum groupinstall -y "Development tools"
+yum install -y python36-devel python36-pip
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install docker-ce
+pip3 install docker-compose requests
+chsh -s /usr/bin/fish
